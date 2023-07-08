9e5b551ccb7fdb2192e4ffc782e6b9c55aeac789
Ainhoa <Ainhoa.Genshin@gmail.com>
2023-07-08 22:37:04 +0200

    Start & End Time try

--- addInstance.js
+++ addInstance.js
@@ -1,23 +1,18 @@
 const addBtn = document.querySelector("#add-btn");
 
 addBtn.addEventListener("click", () => {
-
     addInstance();
-
 });
 
-
 function addInstance() {
-
     // Crear los elementos para la nueva instancia
     var container = document.createElement('div');
     var selectButton = document.createElement('button');
     var timeInput = document.createElement('input');
     var removeButton = document.createElement('button');
     var resetButton = document.createElement('button');
-  
+
     // Configurar los atributos y contenido de los elementos
-    
     container.classList.add('instances');
     selectButton.textContent = '→';
     resetButton.textContent = '↻';
@@ -26,32 +21,21 @@ function addInstance() {
     timeInput.placeholder = '00h 00m 00s 000ms';
     timeInput.disabled = true;
 
-    
     removeButton.addEventListener("click", () => {
         container.remove();
     });
 
-    
-    resetButton.addEventListener ("click", () => {
+    resetButton.addEventListener("click", () => {
         timeInput.value = '00h 00m 00s 000ms';
     });
 
-    // Agregar los elementos al container principal
+    // Agregar los elementos al contenedor principal
     container.appendChild(selectButton);
     container.appendChild(timeInput);
     container.appendChild(resetButton);
     container.appendChild(removeButton);
 
-    // Obtener el container de instancias y agregar la nueva instancia
+    // Obtener el contenedor de instancias y agregar la nueva instancia
     var instancesContainer = document.getElementById('instances-container');
     instancesContainer.appendChild(container);
-  }
-
-
-var resetButton = document.getElementById('reset-btn');
-var timeText = document.getElementById('time-text');
-
-
-resetButton.addEventListener ("click", () => {
-  timeText.value = '00h 00m 00s 000ms';
-});
+}
--- popup.js
+++ popup.js
@@ -2,27 +2,28 @@ const getExactTimeBtn = document.querySelector("#exact-time-btn");
 const startTimerBtn = document.querySelector("#start-time-btn");
 const endTimerBtn = document.querySelector("#end-time-btn");
 const resetBtn = document.querySelector("#reset-btn");
+const timeText = document.querySelector("#time-text");
 
-obj = {
-    startTime: 0,
-    endTime: 0,
+let obj = {
+    startTime: null,
+    endTime: null,
 };
 
 getExactTimeBtn.addEventListener("click", () => {
-    saveDataToLocalStorage();
     injectScript();
 });
 
 startTimerBtn.addEventListener("click", () => {
-    obj.startTime = timeText.value;
-
-    timeText.value = 0;
+    obj.startTime = timeText.value.trim();
+    timeText.value = '00h 00m 00s 000ms';
+    saveDataToLocalStorage();
 });
 
 endTimerBtn.addEventListener("click", () => {
-    obj.endTime = timeText.value;
-
-    timeText.value = 0;
+    obj.endTime = timeText.value.trim();
+    timeText.value = '00h 00m 00s 000ms';
+    saveDataToLocalStorage();
+    calculateTime();
 });
 
 function onLoad() {
@@ -34,48 +35,75 @@ window.onbeforeunload = saveDataToLocalStorage;
 
 function saveDataToLocalStorage() {
     chrome.storage.local.set({ timeData: obj });
-    chrome.storage.local.set({ actualTime: timeText.value });
 }
 
 function getDataFromLocalStorage() {
     chrome.storage.local.get("timeData", function (result) {
-        obj = result.timeData;
-    });
-
-    chrome.storage.local.get("actualTime", function (result) {
-        timeText.value = result.actualTime;
+        if (result.timeData) {
+            obj = result.timeData;
+        }
     });
 }
 
 function calculateTime() {
-    let time = obj.endTime - obj.startTime;
-    alert(time);
+    if (obj.startTime && obj.endTime) {
+        const startTime = parseTime(obj.startTime);
+        const endTime = parseTime(obj.endTime);
+
+        if (startTime !== null && endTime !== null) {
+            const timeDifference = endTime - startTime;
+
+            const hours = Math.floor(timeDifference / 3600000);
+            const minutes = Math.floor((timeDifference % 3600000) / 60000);
+            const seconds = Math.floor((timeDifference % 60000) / 1000);
+            const milliseconds = timeDifference % 1000;
+
+            const calculatedTime = `${padZero(hours)}h ${padZero(minutes)}m ${padZero(seconds)}s ${padZero(milliseconds)}ms`;
+            document.getElementById("calculated-time").value = calculatedTime;
+        } else {
+            alert("Invalid time format. Please set the time in the format '00h 00m 00s 000ms'.");
+        }
+    } else {
+        alert("Please set both start time and end time.");
+    }
+}
+
+function parseTime(timeString) {
+    const regex = /(\d+)h\s+(\d+)m\s+(\d+)s\s+(\d+)ms/;
+    const match = timeString.match(regex);
+
+    if (match) {
+        const hours = parseInt(match[1], 10);
+        const minutes = parseInt(match[2], 10);
+        const seconds = parseInt(match[3], 10);
+        const milliseconds = parseInt(match[4], 10);
+
+        return hours * 3600000 + minutes * 60000 + seconds * 1000 + milliseconds;
+    }
+
+    return null;
+}
+
+function padZero(number) {
+    return number.toString().padStart(2, "0");
 }
 
 function injectScript() {
-    chrome.tabs
-        .query({ active: true, currentWindow: true })
-        .then(function (tabs) {
-            var activeTab = tabs[0];
-            var activeTabId = activeTab.id;
+    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+        var activeTab = tabs[0];
+        var activeTabId = activeTab.id;
 
-            return chrome.scripting.executeScript({
-                target: { tabId: activeTabId },
-                // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
-                func: GetExactSecond,
-                // args: ['body']  // you can use this to target what element to get the html for
-            });
-        })
-        .then(function (results) {
-            timeText.value = results[0].result;
-        })
-        .catch(function (err) {
-            alert("No se ha podido obtener el tiempo exacto");
+        return chrome.scripting.executeScript({
+            target: { tabId: activeTabId },
+            func: GetExactSecond,
         });
+    }).then(function (results) {
+        timeText.value = results[0].result;
+    }).catch(function (err) {
+        alert("No se ha podido obtener el tiempo exacto");
+    });
 }
 
 function GetExactSecond(selector) {
-    return document.getElementsByClassName("video-stream html5-main-video")[0]
-        .currentTime;
+    return document.getElementsByClassName("video-stream html5-main-video")[0].currentTime;
 }
-// oscar eres un culero
