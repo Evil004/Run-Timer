7b19bfe38baa76600bec06ae77166d520837f221
Oscar <oscarferna08@gmail.com>
2023-07-12 23:22:12 +0200

    code reorganization

---
+++ README.md
@@ -0,0 +1,4 @@
+# Frame Timer Extension
+
+This extension allows you to get the exact frame of a YouTube video, store it as the start, get a second frame, store it as the end and calculate the time between the two.
+It also allows you to add several segments and get the total time.
--- chromeExtension/scripts/inject.js
+++ chromeExtension/scripts/inject.js
@@ -1,13 +1,9 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
     if (request.message === "getExactTime") {
-
         var time = document.getElementsByClassName(
             "video-stream html5-main-video"
         )[0].currentTime;
 
-        console.log(time);
-
-        sendResponse({ response: time });
+        sendResponse({ time: time });
     }
 });
